94ff745dd442082570eaacecfdbb9ea008303b19
Salvatore <dested@gmail.com>
2017-12-28 18:42:36 -0700

    deploy stuff

---
+++ site/client/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var s3 = require('gulp-s3-upload')();
+var cloudfront = require('gulp-cloudfront-invalidate');
+
+
+gulp.task('deploy', function () {
+    const settings = {
+        distribution: 'E34E71D3Q69PBI', // Cloudfront distribution ID
+        paths: ['/*'],         // Paths to invalidate
+        wait: false                      // Whether to wait until invalidation is completed (default: false)
+    };
+    return gulp.src(['./css/**', './index.html', './dist/bundle.js'])
+        .pipe(s3({
+            Bucket: 'quickga.me',
+            ACL: 'public-read'
+        }, {
+            maxRetries: 5
+        }))
+        .pipe(cloudfront(settings));
+});
--- site/serverCommon/config.ts
+++
@@ -1,3 +0,0 @@
-export class Config {
-    static db = 'mongodb://127.0.0.1:27017';
-}
